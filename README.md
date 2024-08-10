481659694e2f4cedf01b35fe4c452a55c57deec7
Vadymbo-here <vadymbo@i.ua>
2024-08-10 20:49:51 +0300

    Created template and set up basic folder structure

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # car_rental_project
+
+# Setup Frontend
+Start with installing packages
+```yarn```
+And rollup a project with
+```yarn dev```
---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  const name = 'Vadym'
+
+  return (
+    <>
+      <h1>Hello frontend</h1>
+      <p>We are begging our jorney, {name}</p>
+    </>
+  )
+}
+
+export default App
