c4c3a03340cb0e7c34c127e90605d4db675f792a
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2024-11-19 22:45:14 +0200

    refactor configuration classes

--- backend/src/main/java/nulp/cs/carrentalrestservice/configuration/EventAspectConfiguration.java
+++ backend/src/main/java/nulp/cs/carrentalrestservice/configuration/EventAspectConfiguration.java
@@ -2,12 +2,14 @@ package nulp.cs.carrentalrestservice.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.context.event.ApplicationEventMulticaster;
 import org.springframework.context.event.SimpleApplicationEventMulticaster;
 import org.springframework.core.task.SimpleAsyncTaskExecutor;
 
 @Configuration
-public class EventConfiguration {
+@EnableAspectJAutoProxy
+public class EventAspectConfiguration {
     @Bean(name = "applicationEventMulticaster")
     public ApplicationEventMulticaster applicationEventMulticaster() {
         SimpleApplicationEventMulticaster eventMulticaster = new SimpleApplicationEventMulticaster();
