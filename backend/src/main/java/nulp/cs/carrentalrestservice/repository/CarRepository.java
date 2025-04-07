516c6b7db6d4a33132bbe0fa1b896f915a32b628
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2025-04-07 15:55:55 +0300

    add jdbc template repository to find cats by criteria

--- backend/src/main/java/nulp/cs/carrentalrestservice/repository/CarRepository.java
+++ backend/src/main/java/nulp/cs/carrentalrestservice/repository/CarRepository.java
@@ -1,17 +1,9 @@
 package nulp.cs.carrentalrestservice.repository;
 
-import nulp.cs.carrentalrestservice.entity.Brand;
 import nulp.cs.carrentalrestservice.entity.Car;
-import nulp.cs.carrentalrestservice.model.enumeration.CarClass;
-import nulp.cs.carrentalrestservice.model.enumeration.FuelType;
-import nulp.cs.carrentalrestservice.model.enumeration.GearboxType;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
-import java.util.List;
 import java.util.UUID;
 
 @Repository
