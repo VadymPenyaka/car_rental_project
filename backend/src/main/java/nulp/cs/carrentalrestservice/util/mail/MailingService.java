823dd76c8fb5f288e0b1644b63c287b2810b8fd6
VadymPenyaka <112482661+VadymPenyaka@users.noreply.github.com>
2025-05-23 04:25:47 +0300

    change mailing api

--- backend/src/main/java/nulp/cs/carrentalrestservice/util/mail/MailingService.java
+++ backend/src/main/java/nulp/cs/carrentalrestservice/util/mail/MailingService.java
@@ -1,5 +1,7 @@
 package nulp.cs.carrentalrestservice.util.mail;
 
+import java.io.IOException;
+
 public interface MailingService {
     void sendEmail(String receiver, String subject, String text);
 }
