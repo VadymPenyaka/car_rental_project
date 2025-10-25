310b579ae0cd05f86afa55f604c71ca85d655bdf
Vadymbo-here <Vadymbo@i.ua>
2025-10-25 19:50:26 +0200

    added link to main page to mobile version

--- frontend/src/components/header/HeaderInformationMobile.tsx
+++ frontend/src/components/header/HeaderInformationMobile.tsx
@@ -1,13 +1,20 @@
-import Logo from "../ui/Logo";
+import Logo from "../ui/Logo"
 
-import ring_phone from "../../assets/phone_2.svg";
-import location from "../../assets/location.svg";
-import { HeaderNavigationMobile } from "./HeaderNavigationMobile";
+import ring_phone from "../../assets/phone_2.svg"
+import location from "../../assets/location.svg"
+import { HeaderNavigationMobile } from "./HeaderNavigationMobile"
+
+import { useNavigate } from 'react-router'
+
 
 export const HeaderInformationMobile = () => {
+    const navigate = useNavigate()
+
     return (
         <div className="flex justify-between h-full items-center p-2">
-            <Logo />
+            <div onClick={() => navigate("/")} className="cursor-pointer">
+                <Logo />
+            </div>
             <div className="flex">
                 <Logo src={ring_phone} width="24rem" className="p-2" />
                 <Logo src={location} width="20rem" className="p-2" />
