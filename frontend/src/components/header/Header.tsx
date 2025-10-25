414caa08dfdfd511e523d1b07850b3fb5a7798b9
Vadymbo-here <Vadymbo@i.ua>
2025-10-25 19:44:50 +0200

    Fixed header button at mobile

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
 	const authHeader = useAuthHeader()
 	console.log("Auth Header:", authHeader) 
 	return (
-		<div className='w-full h-16 shadow-md lg:h-36'>
+		<div className='w-full shadow-md lg:h-36'>
 			<div className="max-w-5xl mx-auto hidden lg:block">
 				<HeaderInformation />
 				<HeaderNavigation />
--- frontend/src/components/header/HeaderInformationMobile.tsx
+++ frontend/src/components/header/HeaderInformationMobile.tsx
@@ -1,28 +1,18 @@
-import Logo from '../ui/Logo'
+import Logo from "../ui/Logo";
 
-import ring_phone from '../../assets/phone_2.svg'
-import location from '../../assets/location.svg'
-import { HeaderNavigationMobile } from './HeaderNavigationMobile'
+import ring_phone from "../../assets/phone_2.svg";
+import location from "../../assets/location.svg";
+import { HeaderNavigationMobile } from "./HeaderNavigationMobile";
 
 export const HeaderInformationMobile = () => {
-	return (
-		<>
-			<div className="flex justify-between h-full">
-				<Logo />
-				<div className='flex items-center'>
-					<Logo
-						src={ring_phone}
-						width='24rem'
-						className='p-2'
-					/>
-					<Logo
-						src={location}
-						width='20rem'
-						className='p-2'
-					/>
-					<HeaderNavigationMobile />
-				</div>
-			</div>
-		</>
-	)
-}
+    return (
+        <div className="flex justify-between h-full items-center p-2">
+            <Logo />
+            <div className="flex">
+                <Logo src={ring_phone} width="24rem" className="p-2" />
+                <Logo src={location} width="20rem" className="p-2" />
+                <HeaderNavigationMobile />
+            </div>
+        </div>
+    );
+};
